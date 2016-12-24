dc44c2427de99ba3e1dcad1c1f23d8ab70c58064
Shaz <shahebaz13@gmail.com>
2016-12-24 13:05:49 +0530

    Simpler. Better
    Updated over all fludity and inputfit() function

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 $(document).ready(function(){
 $("#glog").show();
 
- $("#glog").delay(3000).animate({ top: '8%' }, 700  );
+
+
+
 
 
 $("#s").hide();
 
-setTimeout(function() {
+setTimeout(function () {
 	$("#s").fadeIn(600);
-	$("#s").typed({
-		  strings: ["Simple.","Quick.","Search.",""],
-						typeSpeed: 120,
-						contentType: 'html',
-						backSpeed: 50
-				});
-
-}, 3500);
-
 $('#s').inputfit();
+}, 2000); // Execute something() 1 second later.
+
+
+
+$("#s").keyup(function(event){
+		if(event.keyCode == 13){
+
+			window.location = "https://www.google.co.in/?gfe_rd=cr&ei=XB9eWKidKvLx8AfvkIr4Cw#q=" + $('#s').val();
+		}
+
+
+});
+
+
+
 
  }
 );
